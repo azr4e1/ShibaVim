b970e5b4af91ab4435c1c70f03948bf9e8e01e76
Personal <drumondlorenzo@posteo.net>
2023-01-22 20:37:00 +0000

    Minor change

--- lua/plugins/wiki-create.lua
+++ lua/plugins/wiki-create.lua
@@ -3,11 +3,11 @@ local picker = require('plugins.wiki-picker')
 
 M.create_wiki = function()
     local name = vim.fn.input("Name of Wiki: ")
-    name = '/' .. name .. "_wiki"
     if name == "" then
         print("No valid name inserted")
         return
     end
+    name = '/' .. name .. "_wiki"
     local full_name = picker.config.projectsFolder .. name
     local index = full_name .. '/index.wiki'
     vim.api.nvim_call_function('system', {'mkdir ' .. full_name})
