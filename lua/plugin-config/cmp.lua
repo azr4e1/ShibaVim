d494e8093d4b8d974884e1a4d9daf73047569cd2
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-04-16 17:20:21 +0100

    Create manual completion mode with tab overload

--- lua/plugin-config/cmp.lua
+++ lua/plugin-config/cmp.lua
@@ -1,11 +1,31 @@
 local cmp = require'cmp'
-local compare = cmp.config.compare
 local luasnip = require'luasnip'
 require("luasnip.loaders.from_vscode").lazy_load()
 
 local check_backspace = function()
     local col = vim.fn.col "." - 1
     return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+local abort = function()
+    cmp.abort()
+    cmp.core:reset()
+end
+
+local abort_or_close = function()
+    if cmp.visible() then
+        abort()
+    else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, true, true), 'n', true)
+    end
+end
+
+local cmdline_cr = function()
+    if cmp.visible() then
+        cmp.confirm({select=false})
+    else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
+    end
 end
 
 --   פּ ﯟ   some other good icons
@@ -39,44 +59,27 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
+    completion = {
+        autocomplete = false,
+        completeopt='menuone'
+    },
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
     mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
         ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
         ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
         ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ["<ESC>"] = cmp.mapping {
-            i = cmp.mapping.abort(),
-            c = function()
-                if cmp.visible() then
-                    cmp.abort()
-                else
-                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-c>', true, true, true), 'n', true)
-                end
-            end
-        },
+        ["<ESC>"] = cmp.mapping(abort_or_close, {"i", "c"}),
         -- Accept currently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
         -- ["<CR>"] = cmp.mapping.confirm { select = false },
         ["<CR>"] = cmp.mapping {
             i = cmp.mapping.confirm { select = false },
-            c = function()
-                    if cmp.visible() then
-                        if cmp.get_active_entry() ~= nil then
-                            cmp.confirm({select=false})
-                        else
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
-                        end
-                    else
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
-                    end
-                end
+            c = cmdline_cr
         },
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
@@ -85,8 +88,8 @@ cmp.setup {
                 luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
+            elseif not check_backspace() then
+                cmp.complete()
             else
                 fallback()
             end
@@ -112,7 +115,6 @@ cmp.setup {
         format = function(entry, vim_item)
             -- Kind icons
             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-            -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             vim_item.menu = ({
                 nvim_lsp = "[LSP]",
                 luasnip = "[Snippet]",
@@ -183,20 +185,3 @@ cmp.setup.filetype({'vimwiki'}, {
         { name = 'omni' },
     }
 })
-
--- cmp.setup.filetype({'python'}, {
---   sources = {
---     { name = "jupynium", priority = 1000 },  -- consider higher priority than LSP
---     { name = "nvim_lsp", priority = 100 },
---     -- ...
---   },
---   sorting = {
---     priority_weight = 1.0,
---     comparators = {
---       compare.score,            -- Jupyter kernel completion shows prior to LSP
---       compare.recently_used,
---       compare.locality,
---       -- ...
---     },
---   },
--- })
