ee7165dc61fc65bd1d21a9034c39b9ff21ab46bb
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-05-01 18:20:22 +0100

    Install treesitter language parser synchronously

--- lua/plugin-config/treesitter.lua
+++ lua/plugin-config/treesitter.lua
@@ -4,7 +4,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
 
     -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- List of parsers to ignore installing
     --ignore_install = { "javascript" },
