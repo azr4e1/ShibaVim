386e164afbe5560ec44c51c480bea2b11f49e854
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-05-15 09:08:47 +0000

    Refactor

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -55,36 +55,36 @@ vim.g.loaded_netrwPlugin = 1
 -----------------------------------------
 require("tokyonight").setup({
     transparent = true,
-  on_highlights = function(hl, c)
-    local prompt = "#2d3149"
-    hl.TelescopeNormal = {
-      bg = c.bg_dark,
-      fg = c.fg_dark,
-    }
-    hl.TelescopeBorder = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePromptNormal = {
-      bg = prompt,
-    }
-    hl.TelescopePromptBorder = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePromptTitle = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePreviewTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopeResultsTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-  end,
+    on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+            bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+        }
+    end,
 })
 vim.api.nvim_command("colorscheme tokyonight")
 vim.api.nvim_set_hl(0, 'Error', {fg="red"})
