b17f927afa4b9c1b2438468ffe3ffc639200e329
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-07-05 22:33:47 +0100

    Fixed all plugins to a commit

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -27,25 +27,25 @@ vim.cmd([[
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use {'wbthomason/packer.nvim',
-         lock=false}
+         commit='1d0cf98a561f7fd654c970c49f917d74fafe1530'}
     -- treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         event = 'VimEnter',
         run = ':TSUpdate',
-        lock=false,
+        commit='1bf573896c7854ca5827074685cd53e81eb30cef',
         config = function()
             require'lazy.treesitter'.setup()
     end,
     }
     use {'nvim-treesitter/playground',
         after = 'nvim-treesitter',
-        lock=false}
+        commit='2b81a018a49f8e476341dfcb228b7b808baba68b'}
     -- Mason
     use {"williamboman/mason.nvim",
-        lock=false}
+        commit='5ad3e113b0c3fde3caba8630599373046f6197e8'}
     use {"williamboman/mason-lspconfig.nvim",
-        lock=false}
+        commit='4f1c72767bec31397d59554f84096909b2887195'}
     -- use {"jay-babu/mason-nvim-dap.nvim",
     --     commit="b4af78dd862e465dfc00ce7a4c0dd811fafb09ed"}
     -- debugger
@@ -67,25 +67,25 @@ return require('packer').startup(function(use)
     -- }
     -- project sidebar
     use {'nvim-tree/nvim-web-devicons',
-         lock=false}
+         commit='9ab9b0b894b2388a9dbcdee5f00ce72e25d85bf9'}
     use {'nvim-tree/nvim-tree.lua',
         opt = true,
         cmd = 'NvimTreeToggle',
         config = function()
             require'nvim-tree'.setup()
         end,
-        lock=false
+        commit='4af572246ce49883e2a52c49203a19984454f2e0'
     }
     -- LSP
     use {'neovim/nvim-lspconfig',
-         lock=false}
+         commit='6b2dafb8f46fe8e59bdb8aa1baed44c942e18e0d'}
     -- focus window
     use {'folke/zen-mode.nvim',
         cmd = 'ZenMode',
         config = function()
             require'plugin-config.zen-mode'.setup()
         end,
-        lock=false
+        commit='eac78cce3eeb05799ec9a984a6ae147db77b88c1'
     }
     -- git signs
     use {'lewis6991/gitsigns.nvim',
@@ -94,7 +94,7 @@ return require('packer').startup(function(use)
         config = function()
             require'plugin-config.git-signs'.setup()
         end,
-        lock=false
+        commit='dc2962f7fce953a2a98e7c6d3d7fc4c1d1f66758'
     }
     -- indentation guides
     use {'lukas-reineke/indent-blankline.nvim',
@@ -102,7 +102,7 @@ return require('packer').startup(function(use)
         config = function()
             require('plugin-config.indent-blankline').setup()
         end,
-        lock=false
+        commit='6061c085417e22f1706c04ceb1cf59bf80d44993'
     }
     -- color visualizer
     use {'norcalli/nvim-colorizer.lua',
@@ -114,38 +114,38 @@ return require('packer').startup(function(use)
         config = function()
             require 'lazy.nvim-colorizer'.setup()
         end,
-        lock=false
+        commit='36c610a9717cc9ec426a07c8e6bf3b3abcb139d6'
     }
     use {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end,
-        lock=false
+        commit='3d22da7f8d72d3eafe1491a7f05ea775b930c481'
     }
     use {"windwp/nvim-autopairs",
-         lock=false}
+         commit='e8f7dd7a72de3e7b6626c050a802000e69d53ff0'}
     use {
         "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        -- tag = "*", -- Use for stability; omit to use `main` branch for the latest features
         config = function()
             require("nvim-surround").setup()
         end,
-        lock=false
+        commit='211eaad7c6d01ef4ac02cba9052b3082ec232101'
     }
     use {'goolord/alpha-nvim',
          config = function ()
              require'plugin-config.alpha'.setup()
          end,
-         lock=false
+         commit='9e33db324b8bb7a147bce9ea5496686ee859461d'
     }
-    use {'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = {'nvim-lua/plenary.nvim'},
-        lock=false
+    use {'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        requires = {{'nvim-lua/plenary.nvim',
+                      commit='102c02903c74b93c705406bf362049383abc87c8'}},
     }
     use {'chrisbra/csv.vim',                   -- CSV
         ft = {'csv'},
-        lock=false
+        commit='15ff93edf5b26c466affbb356e0696b7d6a3b499'
     }
     use {'azr4e1/luatab.nvim',
         config = function()
@@ -153,12 +153,12 @@ return require('packer').startup(function(use)
         end,
     }
     use {'azr4e1/adwaita.nvim'}                  -- adwaita theme
-    use {'vim-scripts/dbext.vim',
-         ft = 'sql',
-         lock=false}                -- SQL
-    use {'nvim-lualine/lualine.nvim', lock=false}            -- statusline
-    use {"folke/which-key.nvim", lock=false}
-    use {'vimwiki/vimwiki', lock=false}
+    -- use {'vim-scripts/dbext.vim',
+    --      ft = 'sql',
+    --      commit=false}                -- SQL
+    use {'nvim-lualine/lualine.nvim', commit='8912bea65de93a56b1f70cdb7c3c26f9cce30394'}            -- statusline
+    use {"folke/which-key.nvim", commit='65b36cc258e857dea92fc11cdc0d6e2bb01d3e87'}
+    use {'vimwiki/vimwiki', commit='65b9f0e68b1c4b6c2023a02b22d11a178e13445e'}
     use {'azr4e1/todo.vim',
          -- opt = true,
          cmd = 'TODO'}
@@ -166,7 +166,7 @@ return require('packer').startup(function(use)
          -- opt = true,
          cmd = 'NNN'}
     use {'azr4e1/terminal.nvim'}
-    use {'nvim-telescope/telescope-ui-select.nvim' }
+    use {'nvim-telescope/telescope-ui-select.nvim', commit='62ea5e58c7bbe191297b983a9e7e89420f581369'}
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
